6c65175458866148661e4b607b071019d29832af
LiXiang <xianglqqo@gmail.com>
2017-08-30 14:13:14 +0800

    1

--- test.js
+++ test.js
@@ -32,119 +32,121 @@ let query = {
    }
 }
 
-let { error, data, filter } = Validator(query,
-   {
-      "tenderName": {
-         "type": String,
-         "name": "标书名称",
-         "allowNull": false,
-         "and": ["tenderNum", "tenderEndTime"],
-      },
-      "tenderNum": Number,
-      "tenderEndTime": {
-         "type": Date,
-         "name": "截标时间",
-         "allowNull": false,
-      },
-      "auth": {
-         "weixin": String,
-      },
-      "beneficiariesName": {
-         "type": String,
-         "name": "xxx",
-         "allowNull": false,
-      },
-      "guaranteeMoney": {
-         "type": Number,
-         "in": [1, 2],
-      },
-      "files": [{
-         "type": String,
-         // "allowNull": false,
-      }],
-      "guaranteeFormat": {
-         "type": Number,
-         "to": Boolean,//conversion
-      },
-      "addressee": {
-         "type": String,
-      },
-      "search": {
-         "type": String,
-         // "or": ["searchField"],
-      },
-      "phone": {
-         "type": "MobilePhone"
-      },
-      "receiveAddress": String,
-      "coupon": {
-         "type": String,
-         method(value) {
-            return { "$gt": value }
-         }
-      },
-      "integral": {
-         "lala": {
-            "type": Number,
-         },
-         "kaka": {
-            "type": Number,
-            "allowNull": false,
-            "in": [1, 3, 8, 6],
-         }
-      },
-      "email": {
-         "type": String,
-         "default": "releaseTime",
-         method(value) {
-            return [value, , , , , "7777"]
-         }
-      },
-      "abc": {
-         "$": {
-            type: Number,
-         }
-      }
-   },
-   {
-      filter() {
-         let { search, email, integral } = this
-         return {
-            "email": email,
-            "integral": integral,
-            "test": {
-               a: 1,
-               b: undefined,
-               c: "",
-               d: null,
-               e: NaN,
-               e: 0,
-            },
-            $or() {
-               if (search.match(/^\d+$/)) {
-                  return [
-                     { tenderNum: new RegExp(search) },
-                     { projectNum: new RegExp(search) },
-                     { tenderProjectNum: new RegExp(search) }
-                  ]
-               } else {
-                  return [
-                     { tenderName: new RegExp(search) },
-                     { projectName: new RegExp(search) },
-                     { tenderProjectName: new RegExp(search) }
-                  ]
-               }
-            },
-            totalAmount() {
-               return {
-                  $gt: /12/,
-                  $lt: 888,
-               }
-            },
-         }
-      }
-   }
-)
+// let { error, data, filter } = Validator(query,
+//    {
+//       "tenderName": {
+//          "type": String,
+//          "name": "标书名称",
+//          "allowNull": false,
+//          "and": ["tenderNum", "tenderEndTime"],
+//       },
+//       "tenderNum": Number,
+//       "tenderEndTime": {
+//          "type": Date,
+//          "name": "截标时间",
+//          "allowNull": false,
+//       },
+//       "auth": {
+//          "weixin": String,
+//       },
+//       "beneficiariesName": {
+//          "type": String,
+//          "name": "xxx",
+//          "allowNull": false,
+//       },
+//       "guaranteeMoney": {
+//          "type": Number,
+//          "in": [1, 2],
+//       },
+//       "files": [{
+//          "type": String,
+//          // "allowNull": false,
+//       }],
+//       "guaranteeFormat": {
+//          "type": Number,
+//          "to": Boolean,//conversion
+//       },
+//       "addressee": {
+//          "type": String,
+//       },
+//       "search": {
+//          "type": String,
+//          // "or": ["searchField"],
+//       },
+//       "phone": {
+//          "type": "MobilePhone"
+//       },
+//       "receiveAddress": String,
+//       "coupon": {
+//          "type": String,
+//          method(value) {
+//             return { "$gt": value }
+//          }
+//       },
+//       "integral": {
+//          "lala": {
+//             "type": Number,
+//          },
+//          "kaka": {
+//             "type": Number,
+//             "allowNull": false,
+//             "in": [1, 3, 8, 6],
+//          }
+//       },
+//       "email": {
+//          "type": String,
+//          "default": "releaseTime",
+//          method(value) {
+//             return [value, , , , , "7777"]
+//          }
+//       },
+//       "abc": {
+//          "$": {
+//             type: Number,
+//          }
+//       }
+//    },
+//    {
+//       filter() {
+//          let { search, email, integral } = this
+//          return {
+//             "email": email,
+//             "integral": integral,
+//             "test": {
+//                a: 1,
+//                b: undefined,
+//                c: "",
+//                d: null,
+//                e: NaN,
+//                e: 0,
+//             },
+//             $or() {
+//                if (search.match(/^\d+$/)) {
+//                   return [
+//                      { tenderNum: new RegExp(search) },
+//                      { projectNum: new RegExp(search) },
+//                      { tenderProjectNum: new RegExp(search) }
+//                   ]
+//                } else {
+//                   return [
+//                      { tenderName: new RegExp(search) },
+//                      { projectName: new RegExp(search) },
+//                      { tenderProjectName: new RegExp(search) }
+//                   ]
+//                }
+//             },
+//             totalAmount() {
+//                return {
+//                   $gt: /12/,
+//                   $lt: 888,
+//                }
+//             },
+//          }
+//       }
+//    }
+// )
+
+let { error, data, filter } = Validator(['12'], [String])
 
 if (error) {
    console.log(error)
